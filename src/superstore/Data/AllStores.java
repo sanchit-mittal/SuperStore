e14543b2143ee5eb0c5fcebd0a62fb9670cc8167
PD <PD@DESKTOP-FJ2BOHA>
2018-11-12 19:54:15 +0530

    Initial Commit - 281018-0802PM commit backup till 121118-0747PM

--- src/superstore/Data/AllStores.java
+++ src/superstore/Data/AllStores.java
@@ -4,18 +4,29 @@
  */
 package superstore.Data;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
  *
  * @author PD
  */
-public class AllStores {
+public class AllStores implements Serializable{
     
     private ArrayList<Store> allstores = new ArrayList<>();
-
+    private int storeID = 1;
+    
     public ArrayList<Store> getAllstores() {
         return allstores;
     }
+    
+    public int getStoreID() {
+        return storeID;
+    }
+    
+    public void incrementStoreID(){
+        this.storeID++;
+    }
+    
     
 }
