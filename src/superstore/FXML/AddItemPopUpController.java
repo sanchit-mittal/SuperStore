2ce1780f6aac9564ea251cce1935dc2d60ca0d84
PD <PD@DESKTOP-FJ2BOHA>
2018-11-14 17:11:07 +0530

    Initial Commit - 281018-0802PM latest

--- src/superstore/FXML/AddItemPopUpController.java
+++ src/superstore/FXML/AddItemPopUpController.java
@@ -17,6 +17,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
+import superstore.Data.Category;
 import superstore.Data.Item;
 import superstore.Data.Warehouse;
 
@@ -76,19 +77,19 @@ public class AddItemPopUpController implements Initializable {
         
         //CHEAT
         
-//        this.warehouse.addCategory("test@category1");
-//        this.warehouse.addCategory("test@category2");
-//        this.warehouse.addCategory("test@category3");
-//           
-//        Category c = this.warehouse.getCategories().get(0);
-//        this.warehouse.addSub_Category("test@subcategory1",c);
-//        this.warehouse.addSub_Category("test@subcategory2",c);
-//        this.warehouse.addSub_Category("test@subcategory3",c);
-//        
-//        this.warehouse.addItem(this.warehouse.getCategories().get(0) , this.warehouse.getCategories().get(0).getSubcategories().get(0));
-//        this.warehouse.getItems().get(0).setName("Test@Item1");
-//        this.warehouse.addItem(this.warehouse.getCategories().get(0) , this.warehouse.getCategories().get(0).getSubcategories().get(0));
-//        this.warehouse.getItems().get(1).setName("Test@Item2");
+        this.warehouse.addCategory("test@category1");
+        this.warehouse.addCategory("test@category2");
+        this.warehouse.addCategory("test@category3");
+           
+        Category c = this.warehouse.getCategories().get(0);
+        this.warehouse.addSub_Category("test@subcategory1",c);
+        this.warehouse.addSub_Category("test@subcategory2",c);
+        this.warehouse.addSub_Category("test@subcategory3",c);
+        
+        this.warehouse.addItem(this.warehouse.getCategories().get(0) , this.warehouse.getCategories().get(0).getSubcategories().get(0));
+        this.warehouse.getItems().get(0).setName("Test@Item1");
+        this.warehouse.addItem(this.warehouse.getCategories().get(0) , this.warehouse.getCategories().get(0).getSubcategories().get(0));
+        this.warehouse.getItems().get(1).setName("Test@Item2");
         //
         
         
